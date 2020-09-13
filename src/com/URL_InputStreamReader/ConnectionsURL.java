3bad1dd8a2978ae912b54a6aa9be6fc21b18a55e
TranKai <quyettran057@gmail.com>
2020-09-13 17:37:47 +0700

    URL Connections

--- src/com/URL_InputStreamReader/ConnectionsURL.java
+++ src/com/URL_InputStreamReader/ConnectionsURL.java
@@ -11,22 +11,19 @@ public class ConnectionsURL {
 
 	public static void main(String[] args) {
 		try {
-			URL url = new URL("http://qldt.hanu.vn/");
+			URL url = new URL("https://github.com/kaiTran56/URLConnection");
 			URLConnection urlConnection = url.openConnection();
-			urlConnection.setDoOutput(true);
+			urlConnection.setDoInput(true);
 			urlConnection.connect();
-			BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			BufferedReader input = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 			String line = "";
 			while (line != null) {
-				line = inputStream.readLine();
+				line = input.readLine();
 				System.out.println(line);
 			}
-			inputStream.close();
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
